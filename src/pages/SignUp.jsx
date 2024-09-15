5e571563b7536446fa65f713b422c3c0e7d3a08e
MuhammadAsad56 <ar535363@gmail.com>
2024-09-15 08:18:10 -0700

    Updated

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -50,7 +50,7 @@ const SignUp = () => {
     
     return (
         <>
-            <div className='w-full h-screen bg-gray-100 flex items-center justify-center'>
+            {/* <div className='w-full h-screen bg-gray-100 flex items-center justify-center'>
                 <div className='flex flex-col mb-14 items-center justify-center rounded-lg bg-purple-400 w-1/3 h-fit'>
                     <h2 className='text-center text-3xl my-10 text-white'>Sign Up</h2>
                     <FormSec onClick={handleSignInGoogle} errMessage={errMessage} onclick={handleSignIn} text={"Sign Up"} namefield={<Form.Item
@@ -67,7 +67,38 @@ const SignUp = () => {
                     </Form.Item>}/>
                 </div>
 
-            </div>
+            </div> */}
+<div className='w-full h-screen bg-gray-100 flex items-center justify-center p-4'>
+    <div className='flex flex-col items-center justify-center rounded-lg bg-purple-400 w-full max-w-xs h-fit p-6 md:p-8 shadow-lg'>
+        <h2 className='text-center text-xl md:text-3xl my-4 md:my-6 text-white font-semibold'>Sign Up</h2>
+        <FormSec 
+            onClick={handleSignInGoogle} 
+            errMessage={errMessage} 
+            onclick={handleSignIn} 
+            text={"Sign Up"} 
+            namefield={
+                <Form.Item
+                    label="Name"
+                    name="name"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your name!',
+                        },
+                    ]}
+                >
+                    <Input 
+                        className='w-full p-2 rounded-md text-sm' 
+                        onChange={(e) => setAuthValues((prev) => ({...prev, name: e.target.value}))} 
+                    />
+                </Form.Item>
+            }
+        />
+    </div>
+</div>
+
+
+
         </>
     )
 }
